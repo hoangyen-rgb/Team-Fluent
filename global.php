acba2394e0230d6a9692dbe5bf01283b6f38b15a
Chick-kun <ThanhCuong0794294868@gmail.com>
2023-11-14 20:28:20 +0700

    cấu trúc php dao

---
+++ global.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    $ROOT_URL = "/chickcuisine";
+    $CONTENT_URL = "$ROOT_URL/content";
+    $ADMIN_URL = "$ROOT_URL/admin";
+    $SITE_URL = "$ROOT_URL/site";
+    $IMAGE_DIR = "$ROOT_URL/content/image";
+    $VIEW_NAME = "index.php";
+    $MESSAGE = "";
+?>
